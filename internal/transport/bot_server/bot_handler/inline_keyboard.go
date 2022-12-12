25f7caae007340a3777b879527879d93982c94ba
emilislamov <emil.islamov@mircod.com>
2022-12-12 06:11:39 +0100

    add function for generate keyboard

---
+++ internal/transport/bot_server/bot_handler/inline_keyboard.go
@@ -0,0 +1,21 @@
+package bot_handler
+
+import (
+	"github.com/binance-converter/telegram-bot/core"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+const (
+	cancelButton = "Cancel"
+)
+
+func generateInlineKeyboardWithCancel[T string | core.CurrencyCode | core.CurrencyBank](buttons []T) tgbotapi.
+	InlineKeyboardMarkup {
+	var rows []tgbotapi.InlineKeyboardButton
+	buttons = append(buttons, cancelButton)
+	for _, button := range buttons {
+		rows = append(rows, tgbotapi.NewInlineKeyboardButtonData(string(button), string(button)))
+	}
+
+	return tgbotapi.NewInlineKeyboardMarkup(rows)
+}
